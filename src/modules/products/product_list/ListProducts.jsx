007bfd27e235dfef190a300d3d2975ddb8ad3983
RodriguezJose88 <palma7876@gmail.com>
2023-11-08 13:38:14 -0600

    responsive category

--- src/modules/products/product_list/ListProducts.jsx
+++ src/modules/products/product_list/ListProducts.jsx
@@ -1,13 +1,17 @@
 import SideBar from "../../../components/generals/Siderbar";
 import Category from "./list_components/Category";
 import Slider from "./list_components/Slider";
+import { categorias } from "./list_components/categorydata";
+
 const ListProducts = () => {
+    
     return (
 <div>
 <SideBar/>
 
 <Slider/>
-<Category/>
+
+<Category title="Categorías" data = {categorias}/>
 
 <h1>ACA IRAN LOS PRODUCTOS</h1>
 </div>
--- src/modules/products/product_list/list_components/Category.jsx
+++ src/modules/products/product_list/list_components/Category.jsx
@@ -1,8 +1,39 @@
-const Category = () => {
+import React from 'react';
+import '../../../../assets/css/category.css';
+
+
+
+const Category = (props) => {
+
+    if (!props.data || !Array.isArray(props.data) || props.data.length === 0) {
+        return (
+            <div className="explore__container">
+                <h1>{props.title}</h1>
+                <p>Datos no disponibles</p>
+            </div>
+        );
+    }
+
     return ( 
-        <div>
-        <section></section>
-                </div>
+        <div className="explore__container">
+        <h1>
+            {props.title}
+        </h1>
+        <div className="explore__container--inner">
+            {props.data.map((item, index) => {
+                return (
+                    <div key={index} className="explore__container--inner-card  btn btn-outline-primary m-1">
+                        <img src={item.url} alt="item" style={
+                            {
+                                height:40,
+                            }
+                        } />
+                        <h2>{item.title}</h2>
+                    </div>
+                );
+            })}
+        </div>
+    </div>
      );
 }
  
