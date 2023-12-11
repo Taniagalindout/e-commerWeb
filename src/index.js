1ac759d724efbeb327ceee612ef709472b11b3e9
Taniagalindout <20203tn155@utez.edu.mx>
2023-12-11 02:16:38 -0600

    update: sw registration

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Registra el Service Worker aquí
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/serviceWorker.js").then((registration) => {
-      console.log("ServiceWorker registered with scope:", registration.scope);
-    }).catch((error) => {
-      console.log("ServiceWorker registration failed:", error);
-    });
-  });
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -23,6 +15,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
